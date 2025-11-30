e58ef7b63a87c801376e4a084dba869f4bb78465
kulichsss <dankul10@yandex.ru>
2025-11-30 17:57:41 +0300

    CheckHeaderTest

---
+++ src/test/java/tests/CheckHeaderTest.java
@@ -0,0 +1,34 @@
+package tests;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Header;
+import io.restassured.http.Headers;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class CheckHeaderTest {
+
+    @Test
+    public void CheckHeaderTest() {
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/homework_header")
+                .andReturn();
+        Headers headers = response.getHeaders();
+        System.out.println(headers);
+
+        for (Header header: headers) {
+            String nameHeader = header.getName();
+            String valueHeader = header.getValue();
+
+            assertNotNull(headers, "Хедеров нет в ответе на запрос");
+            assertEquals(
+                    valueHeader,
+                    response.getHeader(nameHeader),
+                    "Хедера с именем " + nameHeader + " нет в ответе запроса"
+            );
+        }
+    }
+}
